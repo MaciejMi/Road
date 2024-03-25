f7f212a69d80bd034fcf9b269493a452e54dc8a5
MaciejMi <maciejmich22@gmail.com>
2024-03-25 19:37:15 +0100

    add only near button and by distance button and make functionality to this program

--- src/road/Road.java
+++ src/road/Road.java
@@ -31,6 +31,10 @@ public class Road extends javax.swing.JFrame {
         g = jPanel1.getGraphics();
         jPanel1.paintComponents(g);
         random = new Random();
+        
+        jButton3.setEnabled(false);
+        jButton4.setEnabled(false);
+
     }
 
     /**
@@ -46,6 +50,8 @@ public class Road extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -77,6 +83,20 @@ public class Road extends javax.swing.JFrame {
             }
         });
 
+        jButton3.setText("Only near");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("By Distance");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -85,7 +105,11 @@ public class Road extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(31, 31, 31)
                 .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 285, Short.MAX_VALUE)
+                .addGap(35, 35, 35)
+                .addComponent(jButton3)
+                .addGap(43, 43, 43)
+                .addComponent(jButton4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
                 .addComponent(jButton2)
                 .addGap(35, 35, 35))
         );
@@ -96,40 +120,171 @@ public class Road extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(jButton2)
+                    .addComponent(jButton3)
+                    .addComponent(jButton4))
                 .addGap(20, 20, 20))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        
+      
         /* removing previous points*/
         g.setColor(jPanel1.getBackground());
         g.fillRect(0, 0, jPanel1.getWidth(), jPanel1.getHeight());
         g.setColor(Color.black);
         
+        boolean isStart = true;
         
         /* filling points */
         for (int i = 0; i < points.length; i++){
+            g.setColor(Color.black);
+            
             int x  = random.nextInt(RANDOM_SCOPE) + 1;
             int y = random.nextInt(RANDOM_SCOPE) + 1;
 
             points[i][0] = x;
             points[i][1] = y;
 
-            g.fillOval(x, y, 10, 10);
+            if (isStart){
+                g.setColor(Color.blue);
+                isStart = false;
+            }
+            
+            if (i == points.length - 1){
+                g.setColor(Color.red);
+            }
+            
+            g.fillOval(x - 5, y - 5, 10, 10);
             
             isTouched[i] = false;
        }
        isTouched[0] = true;
-        
+       jButton3.setEnabled(true);
+       jButton4.setEnabled(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
       jPanel1.repaint();
+      jButton3.setEnabled(false);
+      jButton4.setEnabled(false);
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        g.setColor(Color.black);
+        
+        int lastX = points[points.length - 1][0];
+        int lastY = points[points.length - 1][1];
+        
+        int currentX = points[0][0];
+        int currentY = points[0][1];
+        
+        int nextX = -1;
+        int nextY = -1;
+        int nextIndex = -1;
+        
+        boolean isEnd = false;
+        
+        for (int[] point: points){
+            int x = point[0];
+            int y = point[1];
+            
+            float lastDistance = Integer.MAX_VALUE;
+            
+            for (int i = 0; i < points.length; i++){
+                if(isTouched[i]){
+                    continue;
+                }
+                
+                float currentDistance = (float) Math.sqrt((points[i][0] - currentX) * (points[i][0] - currentX) + (points[i][1] - currentY) * (points[i][1] - currentY));
+                
+                if (currentDistance < lastDistance){
+                    nextX = points[i][0];
+                    nextY = points[i][1];
+                    nextIndex = i;
+                    lastDistance = currentDistance;
+                }
+            }
+            
+            isTouched[nextIndex] = true;
+           
+            if (!isEnd){
+                g.drawLine(currentX, currentY, nextX, nextY);
+                currentX = nextX;
+                currentY = nextY;
+            }
+            
+            if (nextIndex == ARRAY_LENGTH - 1){
+                isEnd = true;
+            }
+        }
+        
+       jButton3.setEnabled(false);
+       jButton4.setEnabled(false);
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        g.setColor(Color.black);
+        
+        int lastX = points[points.length - 1][0];
+        int lastY = points[points.length - 1][1];
+        
+        int currentX = points[0][0];
+        int currentY = points[0][1];
+        
+        int nextX = -1;
+        int nextY = -1;
+        int nextIndex = -1;
+        
+        boolean isEnd = false;
+        
+        float DISTANCE_BETWEEN_START_AND_STOP = (float) Math.sqrt((lastX - currentX) * (lastX - currentX) + (lastY - currentY) * (lastY - currentY));
+        float currentDistanceToStop = DISTANCE_BETWEEN_START_AND_STOP;
+        
+        for (int[] point: points){
+            int x = point[0];
+            int y = point[1];
+            
+            float lastDistance = Integer.MAX_VALUE;
+            
+            for (int i = 0; i < points.length; i++){
+                if(isTouched[i]){
+                    continue;
+                }
+                
+                float currentDistance = (float) Math.sqrt((points[i][0] - currentX) * (points[i][0] - currentX) + (points[i][1] - currentY) * (points[i][1] - currentY));
+                
+                currentDistanceToStop = (float) Math.sqrt((lastX - currentX) * (lastX - currentX) + (lastY - currentY) * (lastY - currentY));
+                float nextDistanceToStop = (float) Math.sqrt((lastX - points[i][0]) * (lastX - points[i][0]) + (lastY - points[i][1]) * (lastY - points[i][1]));
+              
+                if (nextDistanceToStop < currentDistanceToStop){
+                    if (currentDistance < lastDistance){
+                        nextX = points[i][0];
+                        nextY = points[i][1];
+                        nextIndex = i;
+                        lastDistance = currentDistance;
+                    }
+                }
+            }
+            
+            isTouched[nextIndex] = true;
+           
+            if (!isEnd){
+                g.drawLine(currentX, currentY, nextX, nextY);
+                currentX = nextX;
+                currentY = nextY;
+            }
+            
+            if (nextIndex == ARRAY_LENGTH - 1){
+                isEnd = true;
+            }
+        }
+       jButton3.setEnabled(false);
+       jButton4.setEnabled(false);
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -172,6 +327,8 @@ public class Road extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
